58169a154231a8c15fbeb0e4a5fc69cca7745215
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-03-23 21:06:03 +0100

    Initial commit

---
+++ src/main/java/com/babalola/brainiacbot/BrainiacBotApplication.java
@@ -0,0 +1,13 @@
+package com.babalola.brainiacbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainiacBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainiacBotApplication.class, args);
+	}
+
+}
